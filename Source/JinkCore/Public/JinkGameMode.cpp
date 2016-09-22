1f39b9548bf40eca59e817bd39f98f0e689c28a9
muit <miguel_3c@hotmail.com>
2016-09-22 11:41:37 +0200

    Initial Core Commit

---
+++ Source/JinkCore/Public/JinkGameMode.cpp
@@ -0,0 +1,13 @@
+// Copyright 2015-2016 Piperift, All Rights Reserved.
+
+#include "JinkCorePrivatePCH.h"
+#include "JinkGameMode.h"
+
+AJinkGameMode::AJinkGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	//if (PlayerPawnBPClass.Class != NULL)
+	//{
+	//}
+}
---
+++ Source/JinkCore/Public/JinkGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "JinkGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AJinkGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AJinkGameMode();
+};
+
+
+
