5f98729e8e501015f4a8d1dc92842aa8ae4ffceb
muit <miguel_3c@hotmail.com>
2016-09-27 15:49:14 +0200

    Entity Movement States

--- Source/JinkCore/Private/Entities/Entity.cpp
+++ Source/JinkCore/Private/Entities/Entity.cpp
@@ -9,7 +9,13 @@ AEntity::AEntity()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	live = 100;
+	Live = 100;
+	
+	MovementState = EMovementState::MS_Walk;
+	WalkSpeed = 250;
+	RunSpeed = 400;
+
+	UpdateMovementSpeed();
 }
 
 // Called when the game starts or when spawned
@@ -27,7 +33,40 @@ void AEntity::Tick( float DeltaTime )
 
 bool AEntity::IsAlive() const
 {
-	return live > 0;
+	return Live > 0;
+}
+
+void AEntity::Walk()
+{
+	MovementState = EMovementState::MS_Walk;
+	UpdateMovementSpeed();
+}
+
+void AEntity::Run()
+{
+	MovementState = EMovementState::MS_Run;
+	UpdateMovementSpeed();
+}
+
+void AEntity::UpdateMovementSpeed()
+{
+	UCharacterMovementComponent* _CharacterMovement = GetCharacterMovement();
+	if (_CharacterMovement) {
+		switch (MovementState) {
+		case EMovementState::MS_None:
+			_CharacterMovement->MaxWalkSpeed = 0;
+			break;
+		case EMovementState::MS_Walk:
+			_CharacterMovement->MaxWalkSpeed = WalkSpeed;
+			break;
+		case EMovementState::MS_Run:
+			_CharacterMovement->MaxWalkSpeed = RunSpeed;
+			break;
+		default:
+			//Don't change anything
+			break;
+		}
+	}
 }
 
 
--- Source/JinkCore/Public/Entities/Entity.h
+++ Source/JinkCore/Public/Entities/Entity.h
@@ -5,6 +5,16 @@
 #include "GameFramework/Character.h"
 #include "Entity.generated.h"
 
+//MovementState to define different types of movements.
+UENUM(BlueprintType)
+enum class EMovementState : uint8
+{
+	MS_None	    UMETA(DisplayName = "None"),
+	MS_Walk 	UMETA(DisplayName = "Walk"),
+	MS_Run  	UMETA(DisplayName = "Run")
+};
+
+
 UCLASS(config = Game, BlueprintType, meta = (ShortTooltip = "An entity is a type of Character that includes the basic alive features."))
 class JINKCORE_API AEntity : public ACharacter
 {
@@ -21,11 +31,32 @@ public:
 	virtual void Tick( float DeltaSeconds ) override;
 
 public:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pawn|Entity")
-	float live;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Entity")
+	float Live;
 
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Entity|Movement")
+	EMovementState MovementState;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Entity|Movement")
+	float WalkSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Entity|Movement")
+	float RunSpeed;
 
 public:
-	UFUNCTION(BlueprintCallable, Category = "Pawn|Entity")
+	UFUNCTION(BlueprintCallable, Category = "Entity")
 	virtual bool IsAlive() const;
+
+	//Set Movement to Walk
+	UFUNCTION(BlueprintCallable, Category = "Entity|Movement")
+	virtual void Walk();
+
+	//Set Movement to Run
+	UFUNCTION(BlueprintCallable, Category = "Entity|Movement")
+	virtual void Run();
+
+	UFUNCTION(BlueprintCallable, Category = "Entity|Movement")
+	virtual void UpdateMovementSpeed();
+
 };
