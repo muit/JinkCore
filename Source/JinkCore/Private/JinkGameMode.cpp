b9bdbd39114ab2e1fc6de0253708759ebadc5c3a
muit <miguel_3c@hotmail.com>
2016-10-18 20:19:54 +0200

    small fix

--- Source/JinkCore/Private/JinkGameMode.cpp
+++ Source/JinkCore/Private/JinkGameMode.cpp
@@ -1,13 +1,11 @@
 // Copyright 2015-2016 Piperift. All Rights Reserved.
 
 #include "JinkCorePrivatePCH.h"
+#include "Entity.h"
 #include "JinkGameMode.h"
 
 AJinkGameMode::AJinkGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	//if (PlayerPawnBPClass.Class != NULL)
-	//{
-	//}
+	DefaultPawnClass = AEntity::StaticClass();
 }
--- Source/JinkCore/Public/JinkGameMode.h
+++ Source/JinkCore/Public/JinkGameMode.h
@@ -1,10 +1,11 @@
 // Copyright 2015-2016 Piperift, All Rights Reserved.
 #pragma once
 #include "GameFramework/GameMode.h"
+#include "Engine/StreamableManager.h"
 #include "JinkGameMode.generated.h"
 
-UCLASS(minimalapi)
-class AJinkGameMode : public AGameMode
+UCLASS()
+class JINKCORE_API AJinkGameMode : public AGameMode
 {
 	GENERATED_BODY()
 
