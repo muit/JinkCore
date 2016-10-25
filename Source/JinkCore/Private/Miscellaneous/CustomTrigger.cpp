d6d01b492695cf00051f22e3cd2e85ad18cdd4a7
muit <miguel_3c@hotmail.com>
2016-10-25 22:36:01 +0200

    CustomTrigger 1/2

---
+++ Source/JinkCore/Private/Miscellaneous/CustomTrigger.cpp
@@ -0,0 +1,11 @@
+// Copyright 2015-2016 Piperift. All Rights Reserved.
+
+#include "JinkCorePrivatePCH.h"
+#include "CustomTrigger.h"
+
+ACustomTrigger::ACustomTrigger(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer) 
+{
+}
+
+
